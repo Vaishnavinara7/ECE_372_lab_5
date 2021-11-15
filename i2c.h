5e8250942c7a83a0fdd73944ce697fde39b9a4b8
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2021-11-15 16:26:27 -0700

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -4,6 +4,7 @@
 void initI2C();
 void StartI2C_Trans(unsigned char SLA);
 void StopI2C_Trans();
+void Write(unsigned char data);
 void Read_from(unsigned char SLA, unsigned char MEMADDRESS);
 unsigned char Read_data();
 
